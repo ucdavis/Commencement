029d2eace52220b2e611f79c8e2d128f412a0b3d
Scott Kirkland <srkirkland@gmail.com>
2010-07-19 16:40:26 +0000

    Added global settings, updated NHProf, configured routes and IoC.
    git-tfs-id: [https://tfs.caes.ucdavis.edu:8088/tfs]$/Commencement/Trunk/Commencement;C7819

--- Commencement/Global.asax.cs
+++ Commencement/Global.asax.cs
@@ -1,9 +1,12 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 using System.Web.Mvc;
-using System.Web.Routing;
+using Castle.Windsor;
+using Commencement.Controllers;
+using Microsoft.Practices.ServiceLocation;
+using MvcContrib.Castle;
+using UCDArch.Web.IoC;
+using UCDArch.Web.ModelBinder;
+using UCDArch.Web.Validator;
 
 namespace Commencement
 {
@@ -12,21 +15,33 @@ namespace Commencement
 
     public class MvcApplication : System.Web.HttpApplication
     {
-        public static void RegisterRoutes(RouteCollection routes)
-        {
-            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
-
-            routes.MapRoute(
-                "Default",                                              // Route name
-                "{controller}/{action}/{id}",                           // URL with parameters
-                new { controller = "Home", action = "Index", id = "" }  // Parameter defaults
-            );
-
-        }
-
         protected void Application_Start()
         {
-            RegisterRoutes(RouteTable.Routes);
+            #if DEBUG
+            HibernatingRhinos.Profiler.Appender.NHibernate.NHibernateProfiler.Initialize();
+            #endif
+
+            xVal.ActiveRuleProviders.Providers.Add(new ValidatorRulesProvider());
+
+            //RegisterRoutes(RouteTable.Routes);
+            new RouteConfigurator().RegisterRoutes();
+
+            ModelBinders.Binders.DefaultBinder = new UCDArchModelBinder();
+
+            IWindsorContainer container = InitializeServiceLocator();
+        }
+
+        private static IWindsorContainer InitializeServiceLocator()
+        {
+            IWindsorContainer container = new WindsorContainer();
+            ControllerBuilder.Current.SetControllerFactory(new WindsorControllerFactory(container));
+
+            container.RegisterControllers(typeof(HomeController).Assembly);
+            ComponentRegistrar.AddComponentsTo(container);
+
+            ServiceLocator.SetLocatorProvider(() => new WindsorServiceLocator(container));
+
+            return container;
         }
     }
 }
