ea07817c10cceefe601a82b1d24bd2979c313f6c
Scott Kirkland <srkirkland@gmail.com>
2010-07-19 19:23:35 +0000

    Added commencement related classes/mappings.
    git-tfs-id: [https://tfs.caes.ucdavis.edu:8088/tfs]$/Commencement/Trunk/Commencement;C7827

---
+++ Commencement.Core/Domain/Commencement.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using UCDArch.Core.NHibernateValidator.Extensions;
+using NHibernate.Validator.Constraints;
+using UCDArch.Core.DomainModel;
+
+namespace Commencement.Core.Domain
+{
+    public class Commencement : DomainObject
+    {
+        [Required]
+        [Length(200)]
+        public virtual string Location { get; set; }
+        
+        [NotNull]
+        public virtual DateTime DateTime { get; set; }
+        
+        [NotNull]
+        [Min(1)]
+        public virtual int TotalTickets { get; set; }
+        
+        [NotNull]
+        public virtual DateTime RegistrationDeadline { get; set; }
+    }
+}
---
+++ Commencement.Core/Domain/CommencementEditor.cs
@@ -0,0 +1,17 @@
+﻿using NHibernate.Validator.Constraints;
+using UCDArch.Core.NHibernateValidator.Extensions;
+using UCDArch.Core.DomainModel;
+
+namespace Commencement.Core.Domain
+{
+    public class CommencementEditor : DomainObject
+    {
+        [Required]
+        [Length(50)]
+        public virtual string LoginId { get; set; }
+        
+        public virtual bool Owner { get; set; }
+
+        public virtual Commencement Commencement { get; set; }
+    }
+}
