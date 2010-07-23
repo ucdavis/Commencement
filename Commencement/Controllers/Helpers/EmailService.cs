3a6aa9652ba76dbaca1d73769532ef01e35cacf7
Lai, Alan <anlai@ucdavis.edu>
2010-07-23 18:23:48 +0000

    modified the email service and added method stubs to handle more processing of each specific type of email being sent out.
    git-tfs-id: [https://tfs.caes.ucdavis.edu:8088/tfs]$/Commencement/Trunk/Commencement;C7889

--- Commencement/Controllers/Helpers/EmailService.cs
+++ Commencement/Controllers/Helpers/EmailService.cs
@@ -3,12 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net.Mail;
 using System.Web;
+using Commencement.Core.Domain;
+using UCDArch.Core.PersistanceSupport;
+using UCDArch.Core.Utils;
 
 namespace Commencement.Controllers.Helpers
 {
     public interface IEmailService
     {
         void SendEmail(string[] to, string body);
+        void SendRegistrationConfirmation(IRepository repository, Registration registration);
+        void SendExtraTicketPetitionConfirmation(IRepository repository, ExtraTicketPetition extraTicketPetition);
+        void SendRegistrationPetitionConfirmation(IRepository repository, RegistrationPetition registrationPetition);
     }
 
     public class EmailService : IEmailService
@@ -17,13 +23,29 @@ namespace Commencement.Controllers.Helpers
         {
             throw new NotImplementedException();
         }
+
+        public void SendRegistrationConfirmation(IRepository repository, Registration registration)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SendExtraTicketPetitionConfirmation(IRepository repository, ExtraTicketPetition extraTicketPetition)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SendRegistrationPetitionConfirmation(IRepository repository, RegistrationPetition registrationPetition)
+        {
+            throw new NotImplementedException();
+        }
     }
 
     public class DevEmailService : IEmailService
     {
+        SmtpClient client = new SmtpClient();
+
         public void SendEmail(string[] to, string body)
         {
-            var client = new SmtpClient();
             var message = new MailMessage();
             message.IsBodyHtml = true;
             //foreach(var address in to) message.To.Add(address);
@@ -31,6 +53,50 @@ namespace Commencement.Controllers.Helpers
             message.Body = body;
 
             client.Send(message);
+        }
+
+        public void SendRegistrationConfirmation(IRepository repository, Registration registration)
+        {
+            Check.Require(repository != null, "Reposity is required.");
+            Check.Require(registration != null, "Registration is required.");
+
+            var message = InitializeMessage();
+            message.Subject = registration.Ceremony.Name + " Registration";
+
+            // add who to mail the message to
+            //message.To.Add(registration.Student.Email);
+            //if (!string.IsNullOrEmpty(registration.Email)) message.To.Add(registration.Email);
+            message.To.Add("anlai@ucdavis.edu");
+
+            // get the latest registration confirmation template
+            var template = repository.OfType<Template>().Queryable.Where(a => a.RegistrationConfirmation).OrderByDescending(a => a.Id).FirstOrDefault();
+            Check.Require(template != null, "No template is available.");
+
+            // process the template text
+
+            var body = string.Empty;
+            message.Body = template.BodyText;
+
+            client.Send(message);
+        }
+
+        public void SendExtraTicketPetitionConfirmation(IRepository repository, ExtraTicketPetition extraTicketPetition)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SendRegistrationPetitionConfirmation(IRepository repository, RegistrationPetition registrationPetition)
+        {
+            throw new NotImplementedException();
+        }
+
+        private MailMessage InitializeMessage()
+        {
+            var message = new MailMessage();
+            message.IsBodyHtml = true;
+            message.Bcc.Add("automatedemail@ucdavis.edu");
+
+            return message;
         }
     }
 }
