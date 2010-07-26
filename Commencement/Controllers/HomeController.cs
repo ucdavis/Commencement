375e49eeecafb232318afae69038e7cfde3375d7
Lai, Alan <anlai@ucdavis.edu>
2010-07-26 19:40:04 +0000

    force authorization now.
    git-tfs-id: [https://tfs.caes.ucdavis.edu:8088/tfs]$/Commencement/Trunk/Commencement;C7899

--- Commencement/Controllers/HomeController.cs
+++ Commencement/Controllers/HomeController.cs
@@ -1,17 +1,20 @@
 ﻿using System.Web.Mvc;
+using Commencement.Controllers.Filters;
 using Commencement.Controllers.Helpers;
 using UCDArch.Web.Attributes;
+using MvcContrib;
 
 namespace Commencement.Controllers
 {
     //[HandleTransactionsManually]
     public class HomeController : ApplicationController
     {
+        [Authorize]
         public ActionResult Index()
         {
             ViewData["Message"] = "Welcome to ASP.NET MVC!";
 
-            var term = TermService.GetCurrent();
+            if (User.IsInRole(RoleNames.RoleAdmin) || User.IsInRole(RoleNames.RoleUser)) return this.RedirectToAction<AdminController>(a => a.Index());
 
             return View();
         }
