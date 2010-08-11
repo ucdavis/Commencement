e991ba1546b1d38abfc331d6fe9de085a6aff794
Lai, Alan <anlai@ucdavis.edu>
2010-08-11 15:39:05 +0000

    git-tfs-id: [https://tfs.caes.ucdavis.edu:8088/tfs]$/Commencement/Trunk/Commencement;C7964

--- Commencement/Controllers/Helpers/EmailService.cs
+++ Commencement/Controllers/Helpers/EmailService.cs
@@ -21,19 +21,60 @@ namespace Commencement.Controllers.Helpers
 
     public class EmailService : IEmailService
     {
+        SmtpClient client = new SmtpClient();
+        LetterGenerator letterGenerator = new LetterGenerator();
+
         public void SendEmail(string[] to, string body)
         {
-            throw new NotImplementedException();
+            var message = new MailMessage();
+            message.IsBodyHtml = true;
+            foreach(var address in to) message.To.Add(address);
+            //message.To.Add("anlai@ucdavis.edu");
+            message.Body = body;
+
+            client.Send(message);
         }
 
         public void SendRegistrationConfirmation(IRepository repository, Registration registration)
         {
-            throw new NotImplementedException();
+            Check.Require(repository != null, "Repository is required.");
+            Check.Require(registration != null, "Registration is required.");
+
+            var message = InitializeMessage();
+            message.Subject = registration.Ceremony.Name + " Registration";
+
+            // add who to mail the message to
+            message.To.Add(registration.Student.Email);
+            if (registration.Email != null) message.To.Add(registration.Email);
+
+            // get the latest registration confirmation template
+            var template = repository.OfType<Template>().Queryable.Where(a => a.TemplateType.Name == StaticValues.Template_RegistrationConfirmation).OrderByDescending(a => a.Id).FirstOrDefault();
+            Check.Require(template != null, "No template is available.");
+
+            // process the template text
+            message.Body = letterGenerator.GenerateRegistrationConfirmation(registration, template);
+
+            client.Send(message);
         }
 
         public void SendAddPermission(IRepository repository, Student student)
         {
-            throw new NotImplementedException();
+            var term = TermService.GetCurrent();
+
+            Check.Require(repository != null, "Repository is required.");
+            Check.Require(student != null, "Student is required.");
+            Check.Require(term != null, "Unable to get current term.");
+
+            var message = InitializeMessage();
+            message.Subject = term.Name + " Commencement Registration";
+            message.To.Add(student.Email);
+
+            var template = repository.OfType<Template>().Queryable.Where(a => a.TemplateType.Name == StaticValues.Template_RegistrationPetition_Approved).OrderByDescending(a => a.Id).FirstOrDefault();
+            Check.Require(template != null, "No template is available");
+
+            message.Body = letterGenerator.GenerateAddPermission(student, template);
+
+            client.Send(message);
         }
 
         public void SendExtraTicketPetitionConfirmation(IRepository repository, ExtraTicketPetition extraTicketPetition)
@@ -44,6 +85,15 @@ namespace Commencement.Controllers.Helpers
         public void SendRegistrationPetitionConfirmation(IRepository repository, RegistrationPetition registrationPetition)
         {
             throw new NotImplementedException();
+        }
+
+        private MailMessage InitializeMessage()
+        {
+            var message = new MailMessage();
+            message.IsBodyHtml = true;
+            message.Bcc.Add("automatedemail@caes.ucdavis.edu");
+
+            return message;
         }
     }
 
