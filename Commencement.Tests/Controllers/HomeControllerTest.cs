7fe41f25595124de6985abc7e7310730d15b8019
Sylvestre, Jason <jsylvestre@ucdavis.edu>
2010-08-16 22:57:53 +0000

    WIP HomeController tests
    git-tfs-id: [https://tfs.caes.ucdavis.edu:8088/tfs]$/Commencement/Trunk/Commencement;C8038

--- Commencement.Tests/Controllers/HomeControllerTest.cs
+++ Commencement.Tests/Controllers/HomeControllerTest.cs
@@ -1,18 +1,65 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.Web.Mvc;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
-using Commencement;
 using Commencement.Controllers;
+//using Commencement.Controllers.Filter;
+using Commencement.Controllers.ViewModels;
+using Commencement.Core.Domain;
+using Commencement.Tests.Core.Helpers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MvcContrib.TestHelper;
+using Rhino.Mocks;
 using UCDArch.Core.PersistanceSupport;
+using UCDArch.Testing;
+using UCDArch.Web.Attributes;
 
 namespace Commencement.Tests.Controllers
 {
     [TestClass]
-    public class HomeControllerTest
+    public class HomeControllerTest : ControllerTestBase<HomeController>
     {
+        private readonly Type _controllerClass = typeof(HomeController);
+        private IRepositoryWithTypedId<Student, string> _studentRepository;
+
+        #region Init
+
+
+        protected override void SetupController()
+        {
+            _studentRepository = MockRepository.GenerateStub<IRepositoryWithTypedId<Student, string>>();
+            Controller = new TestControllerBuilder().CreateController<HomeController>(_studentRepository);
+        }
+        /// <summary>
+        /// Registers the routes.
+        /// </summary>
+        protected override void RegisterRoutes()
+        {
+            new RouteConfigurator().RegisterRoutes();
+        }
+        #endregion Init
+
+        #region Mapping Tests
+        /// <summary>
+        /// Tests the index mapping.
+        /// </summary>
+        [TestMethod]
+        public void TestIndexMapping()
+        {
+            "~/Home/Index".ShouldMapTo<HomeController>(a => a.Index());
+        }
+
+        /// <summary>
+        /// Tests the about mapping.
+        /// </summary>
+        [TestMethod]
+        public void TestAboutMapping()
+        {
+            "~/Home/About".ShouldMapTo<HomeController>(a => a.About());
+        }
+
+        #endregion Mapping Tests
+
         [TestMethod]
         public void Index()
         {
@@ -39,5 +86,11 @@ namespace Commencement.Tests.Controllers
             //// Assert
             //Assert.IsNotNull(result);
         }
+
+        #region Reflection Tests
+
+        
+
+        #endregion Reflection Tests
     }
 }
