b3e6148fdc88c62188b8d2fb7c93ee9708792edc
Darville, Lisa <ldarville@ucdavis.edu>
2010-08-17 17:51:31 +0000

    git-tfs-id: [https://tfs.caes.ucdavis.edu:8088/tfs]$/Commencement/Trunk/Commencement;C8043

---
+++ Commencement.Tests/Controllers/ErrorControllerTests.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using Commencement.Controllers;
+//using Commencement.Controllers.Filter;
+using Commencement.Controllers.ViewModels;
+using Commencement.Core.Domain;
+using Commencement.Tests.Core.Helpers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MvcContrib.TestHelper;
+using Rhino.Mocks;
+using UCDArch.Core.PersistanceSupport;
+using UCDArch.Testing;
+using UCDArch.Web.Attributes;
+
+namespace Commencement.Tests.Controllers
+{
+     [TestClass]
+    public class ErrorControllerTests : ControllerTestBase<ErrorController>
+    {
+         private readonly Type _controllerClass = typeof(ErrorController);
+
+
+        #region Init
+
+        protected override void SetupController()
+        {
+            Controller = new TestControllerBuilder().CreateController<ErrorController>();
+        }
+        /// <summary>
+        /// Registers the routes.
+        /// </summary>
+        protected override void RegisterRoutes()
+        {
+            new RouteConfigurator().RegisterRoutes();
+        }
+
+        #endregion Init
+
+        #region Mapping
+         
+         /// <summary>
+         /// Tests the index mapping.
+         /// </summary>
+         [TestMethod]
+         public void TestIndexMapping()
+         {
+             "~/Error/Index".ShouldMapTo<ErrorController>(a => a.Index(new Commencement.Controllers.ErrorController.ErrorType()));
+         }
+
+        #endregion Mapping
+
+    }
+}
---
+++ Commencement.Tests/Controllers/PetitionControllerTests.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using Commencement.Controllers;
+//using Commencement.Controllers.Filter;
+using Commencement.Controllers.Helpers;
+using Commencement.Controllers.ViewModels;
+using Commencement.Core.Domain;
+using Commencement.Tests.Core.Helpers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MvcContrib.TestHelper;
+using Rhino.Mocks;
+using UCDArch.Core.PersistanceSupport;
+using UCDArch.Testing;
+using UCDArch.Web.Attributes;
+using Commencement.Tests.Core.Extensions;
+
+namespace Commencement.Tests.Controllers
+{
+    [TestClass]
+    public class PetitionControllerTests : ControllerTestBase<PetitionController>
+    {
+        private readonly Type _controllerClass = typeof(PetitionController);
+    
+
+        #region Init
+
+        protected override void SetupController()
+        {
+            Controller = new TestControllerBuilder().CreateController<PetitionController>();
+        }
+        /// <summary>
+        /// Registers the routes.
+        /// </summary>
+        protected override void RegisterRoutes()
+        {
+            new RouteConfigurator().RegisterRoutes();
+        }
+        
+        #endregion Init
+
+        #region Mapping Tests
+        /// <summary>
+        /// Tests the index mapping.
+        /// </summary>
+        [TestMethod]
+        public void TestIndexMapping()
+        {
+            "~/Petition/Index".ShouldMapTo<PetitionController>(a => a.Index());
+        }
+
+        /// <summary>
+        /// Test the DecideExtraTicketPetition mapping.
+        /// </summary>
+        [TestMethod]
+        public void TestDecideExtraTicketPetitionMapping()
+        {
+            "~/Petition/DecideExtraTicketPetition/5".ShouldMapTo<PetitionController>(a => a.DecideExtraTicketPetition(5, true), true);
+        }
+
+        /// <summary>
+        /// Tests the Register mapping.
+        /// </summary>
+        [TestMethod]
+        public void TestRegisterMapping()
+        {
+            "~/Petition/Register".ShouldMapTo<PetitionController>(a => a.Register());
+        }
+
+
+        /// <summary>
+        /// Tests the ExtraTicketPetition get mapping.
+        /// </summary>
+        [TestMethod]
+        public void TestExtraTicketPetitionGetMapping()
+        {
+            "~/Petition/ExtraTicketPetition/5".ShouldMapTo<PetitionController>(a => a.ExtraTicketPetition(5));
+        }
+
+        /// <summary>
+        /// Tests the ExtraTicketPetition put mapping.
+        /// </summary>
+        [TestMethod]
+        public void TestExtraTicketPetitionPutMapping()
+        {
+            "~/Petition/ExtraTicketPetition/5".ShouldMapTo<PetitionController>(a => a.ExtraTicketPetition(5));
+        }
+
+        #endregion Mapping Tests
+
+    }
+}
