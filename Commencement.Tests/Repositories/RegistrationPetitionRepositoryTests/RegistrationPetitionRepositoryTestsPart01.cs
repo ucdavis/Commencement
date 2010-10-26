0877239ba11b3a7ca76e284de8d310ac3eb58ce0
Sylvestre, Jason <jsylvestre@ucdavis.edu>
2010-10-26 16:37:40 +0000

    Added Fluent mapping tests for RegistrationPetition
    git-tfs-id: [https://tfs.caes.ucdavis.edu:8088/tfs]$/Commencement/Trunk/Commencement;C8496

--- Commencement.Tests/Repositories/RegistrationPetitionRepositoryTests/RegistrationPetitionRepositoryTestsPart01.cs
+++ Commencement.Tests/Repositories/RegistrationPetitionRepositoryTests/RegistrationPetitionRepositoryTestsPart01.cs
@@ -1,7 +1,12 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 using Commencement.Core.Domain;
 using Commencement.Tests.Core.Helpers;
+using FluentNHibernate.Testing;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using UCDArch.Data.NHibernate;
 
 namespace Commencement.Tests.Repositories.RegistrationPetitionRepositoryTests
 {
@@ -11,6 +16,94 @@ namespace Commencement.Tests.Repositories.RegistrationPetitionRepositoryTests
     /// </summary>
     public partial class RegistrationPetitionRepositoryTests
     {
+        #region Fluent Mapping Tests
+        [TestMethod]
+        public void TestCanCorrectlyMapAttachment()
+        {
+            #region Arrange
+
+            var id = RegistrationPetitionRepository.Queryable.Max(x => x.Id) + 1;
+            var session = NHibernateSessionManager.Instance.GetSession();
+            var dateToCheck1 = new DateTime(2010, 01, 01);
+            var dateToCheck2 = new DateTime(2010, 01, 02);
+            double? transferUnits = 201;
+
+            LoadCeremony(1);
+            var ceremony = Repository.OfType<Ceremony>().GetById(1);
+
+            //LoadMajorCode(1);
+            var majorCode = MajorCodeRepository.GetById("1");
+
+            var termCode = TermCodeRepository.GetById("1");
+            #endregion Arrange
+
+            #region Act/Assert
+            new PersistenceSpecification<RegistrationPetition>(session, new RegistrationPetitionEqualityComparer())
+                .CheckProperty(c => c.Id, id)
+                .CheckProperty(c => c.Ceremony, ceremony)
+                .CheckProperty(c => c.CompletionTerm, "CompletionTerm")
+                .CheckProperty(c => c.DateDecision, dateToCheck1)
+                .CheckProperty(c => c.DateSubmitted, dateToCheck2)
+                .CheckProperty(c => c.Email, "test@testy.com")
+                .CheckProperty(c => c.ExceptionReason, "Exception reason")
+                .CheckProperty(c => c.FirstName, "FirstName")
+                .CheckProperty(c => c.IsApproved, true)
+                .CheckProperty(c => c.IsPending, false)
+                .CheckProperty(c => c.LastName, "LastName")
+                .CheckProperty(c => c.Login, "Login")
+                .CheckProperty(c => c.MajorCode, majorCode)
+                .CheckProperty(c => c.MI, "I")
+                .CheckProperty(c => c.Pidm, "PIDM")
+                .CheckProperty(c => c.StudentId, "123456789")
+                .CheckProperty(c => c.TermCode, termCode)
+                .CheckProperty(c => c.TransferUnits, transferUnits)
+                .CheckProperty(c => c.TransferUnitsFrom, "FromSomewhere")
+                .CheckProperty(c => c.Units, 10m)                
+                .VerifyTheMappings();
+            #endregion Act/Assert
+        }
+
+        public class RegistrationPetitionEqualityComparer : IEqualityComparer
+        {
+            bool IEqualityComparer.Equals(object x, object y)
+            {
+                if (x is Ceremony && y is Ceremony)
+                {
+                    if (((Ceremony)x).Id == ((Ceremony)y).Id && ((Ceremony)x).Location == ((Ceremony)y).Location)
+                    {
+                        return true;
+                    }
+                    return false;
+                }
+
+                if (x is MajorCode && y is MajorCode)
+                {
+                    if (((MajorCode)x).Name == ((MajorCode)y).Name && ((MajorCode)x).DisciplineCode == ((MajorCode)y).DisciplineCode)
+                    {
+                        return true;
+                    }
+                    return false;
+                }
+
+                if (x is TermCode && y is TermCode)
+                {
+                    if (((TermCode)x).Name == ((TermCode)y).Name && ((TermCode)x).Id == ((TermCode)y).Id)
+                    {
+                        return true;
+                    }
+                    return false;
+                }
+                return x.Equals(y);
+            }
+
+            public int GetHashCode(object obj)
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        #endregion Fluent Mapping Tests
+
         #region Reflection of Database.
 
         /// <summary>
