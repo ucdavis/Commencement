f21e5382a1f33d0a2d07cf1f4e8dbc38eab2b8ec
Lai, Alan <anlai@ucdavis.edu>
2010-11-17 21:20:17 +0000

    added consolidation major to major code
    git-tfs-id: [https://tfs.caes.ucdavis.edu:8088/tfs]$/Commencement/Trunk/Commencement;C8651

--- Commencement.Core/Domain/MajorCode.cs
+++ Commencement.Core/Domain/MajorCode.cs
@@ -9,6 +9,21 @@ namespace Commencement.Core.Domain
         public virtual string DisciplineCode { get; set; }
         
         public virtual College College { get; set; }
+        public virtual MajorCode ConsolidationMajor { get; set; }
+
+        /// <summary>
+        /// returns the real major for those with consolidation codes
+        /// </summary>
+        public virtual string MajorName { 
+            get { return ConsolidationMajor != null ? ConsolidationMajor.Name : Name; } 
+        }
+
+        /// <summary>
+        /// returns the real college for those with consolidation codes
+        /// </summary>
+        public virtual College MajorCollege {
+            get { return ConsolidationMajor != null ? ConsolidationMajor.College : College; }
+        }
     }
 
     public class MajorCodeMap : ClassMap<MajorCode>
@@ -21,6 +36,7 @@ namespace Commencement.Core.Domain
 
             Map(x => x.Name);
             Map(x => x.DisciplineCode);
+            References(x => x.ConsolidationMajor).Column("ConsolidationCode").Cascade.None().Fetch.Join();
 
             References(x => x.College).Column("CollegeCode");
         }
