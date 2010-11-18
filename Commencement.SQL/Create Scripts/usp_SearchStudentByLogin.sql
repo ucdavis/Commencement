2ea3389ed6c42453a62d7f416e6adf427a8aadc8
Lai, Alan <anlai@ucdavis.edu>
2010-11-18 22:00:24 +0000

    updated search by login query
    git-tfs-id: [https://tfs.caes.ucdavis.edu:8088/tfs]$/Commencement/Trunk/Commencement;C8669

--- Commencement.SQL/Create Scripts/usp_SearchStudentByLogin.sql
+++ Commencement.SQL/Create Scripts/usp_SearchStudentByLogin.sql
@@ -7,8 +7,7 @@ GO
 
 CREATE Procedure usp_SearchStudentByLogin
 	(
-		@login varchar(50),
-		@term varchar(6)
+		@login varchar(50)
 	)
 
 AS
@@ -18,42 +17,49 @@ AS
 	set @login = upper(@login)
 	
 	set @tsql = '
-			select spriden_pidm, spriden_id, spriden_first_name, spriden_mi, spriden_last_name, shrlgpa_hours_earned, goremal_email_address, zgvlcfs_majr_code, zgvlcfs_coll_code, shrdgmr_degs_code, ''' + lower(@login)  + ''' loginid, shrttrm_astd_code_end_of_term
-	from (	
-		select * from openquery (sis, ''
-		select spriden_pidm, spriden_id, spriden_first_name, spriden_mi, spriden_last_name, shrlgpa_hours_earned, email.goremal_email_address, zgvlcfs_majr_code
-			, zgvlcfs_coll_code
-			, shrdgmr_degs_code
-			, shrttrm_astd_code_end_of_term
-		from spriden
-			inner join shrlgpa on spriden_pidm = shrlgpa_pidm
-			inner join wormoth on wormoth_pidm = spriden_pidm
-			left outer join (
-				select goremal_pidm, goremal_email_address
-				from goremal
-				where goremal_emal_code = ''''UCD''''
-					and goremal_status_ind = ''''A''''
-			) email on email.goremal_pidm = spriden_pidm
-			inner join zgvlcfs on zgvlcfs_pidm = spriden_pidm
-			left outer join shrdgmr on shrdgmr_pidm = spriden_pidm
-			left outer join shrttrm on shrttrm_pidm = spriden_pidm
-		where spriden_change_ind is null
-			and wormoth_login_id = ''''' + @login + '''''
-			and shrlgpa_gpa_type_ind = ''''O''''
-			and shrlgpa_levl_code in (''''UG'''', ''''U2'''')
-			and shrdgmr_term_code_sturec in (select max(ishrdgmr.shrdgmr_term_code_sturec) from shrdgmr ishrdgmr where shrdgmr.SHRDGMR_PIDM = ishrdgmr.shrdgmr_pidm)
-			and shrlgpa_hours_earned in ( select max(ishrlgpa.shrlgpa_hours_earned) from shrlgpa ishrlgpa where shrlgpa.shrlgpa_pidm = ishrlgpa.shrlgpa_pidm)
-			and shrttrm_term_code in ( select max(ishrttrm.shrttrm_term_code) from shrttrm ishrttrm where shrttrm.shrttrm_pidm = ishrttrm.shrttrm_pidm)
-			and zgvlcfs_levl_code in (''''UG'''', ''''U2'''')
-			and zgvlcfs_term_code_eff in (select max(izgvlcfs.zgvlcfs_term_code_eff) from zgvlcfs izgvlcfs where zgvlcfs.zgvlcfs_pidm = izgvlcfs.zgvlcfs_pidm)
-				'')) student
-	'
+		select * from openquery(sis, ''
+			select spriden_pidm, spriden_first_name, spriden_mi, spriden_last_name
+				, email.goremal_email_address
+				, earnedunits.units earnedunits
+				, currentunits.units currentunits
+				, zgvlcfs_majr_code
+				, zgvlcfs_term_code_eff as lastTerm
+				, shrttrm_astd_code_end_of_term
+			from wormoth 
+				inner join zgvlcfs on wormoth_pidm = zgvlcfs_pidm
+				inner join spriden on wormoth_pidm = spriden_pidm
+				left outer join (
+					select shrttrm_pidm as pidm, shrttrm_astd_code_end_of_term
+					from shrttrm
+					where shrttrm_term_code in (select max(shrttrm_term_code) from shrttrm ishrttrm
+												where ishrttrm.shrttrm_Pidm = shrttrm.shrttrm_pidm)
+				) astd on astd.pidm = wormoth_pidm
+				left outer join (
+					select goremal_pidm, goremal_email_address
+					from goremal
+					where goremal_emal_code = ''''UCD''''
+						and goremal_status_ind = ''''A''''
+				) email on email.goremal_pidm = wormoth_pidm
+				inner join (
+					select shrlgpa_pidm as pidm, shrlgpa_hours_earned units
+					from shrlgpa
+					where shrlgpa_gpa_type_ind = ''''O'''' and shrlgpa_levl_code = ''''UG''''
+				) EarnedUnits on EarnedUnits.pidm = zgvlcfs_pidm
+				left outer join (
+					select sfrstcr_pidm as pidm, sum(sfrstcr_credit_hr) units
+					from sfrstcr
+						left join shrtckn on sfrstcr_pidm = shrtckn_pidm 
+										 and sfrstcr_term_code = shrtckn_term_code
+										 and sfrstcr_crn = shrtckn_crn
+					where shrtckn_pidm is null
+					group by sfrstcr_pidm    
+				) CurrentUnits on CurrentUnits.pidm = wormoth_pidm
+			where wormoth_login_id = ''''' + @login + '''''
+				and zgvlcfs_term_code_eff in (select max(zgvlcfs_term_code_eff) from zgvlcfs izgvlcfs where izgvlcfs.zgvlcfs_pidm = zgvlcfs.zgvlcfs_pidm)
+				and spriden_change_ind is null
+		'')'
 
-	exec (@tsql)
-	
-	
-
-
+exec(@tsql)
 
 
 GO
