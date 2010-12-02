f55eed53cedc96ae2b4f1f2f078b7893597423bb
Sylvestre, Jason <jsylvestre@ucdavis.edu>
2010-12-02 21:19:05 +0000

    Added class and method to check if a ceremony in the same termcode already has majors for a different ceremony.
    git-tfs-id: [https://tfs.caes.ucdavis.edu:8088/tfs]$/Commencement/Trunk/Commencement;C8774

---
+++ Commencement/Controllers/Helpers/CeremonyMajorCheck.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Commencement.Core.Domain;
+using UCDArch.Core.PersistanceSupport;
+
+namespace Commencement.Controllers.Helpers
+{
+    public class CeremonyMajorCheck
+    {
+        public List<MajorCode> Check(Ceremony ceremony, List<MajorCode> majorCodes, IRepository<Ceremony> ceremonyRepository)
+        {
+            var foundMajors = new List<MajorCode>();
+            foreach (var majorCode in majorCodes)
+            {
+                var code = majorCode;
+                if(ceremonyRepository.Queryable.Where(a => a.TermCode == ceremony.TermCode && a.Id != ceremony.Id && a.Majors.Contains(code)).Any())
+                {
+                    foundMajors.Add(code);
+                }
+            }
+            return foundMajors.Count > 0 ? foundMajors : null;
+        }
+    }
+}
