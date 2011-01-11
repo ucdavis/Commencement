7a8a1314f924f0bd7953c31e6395445914314edc
Sylvestre, Jason <jsylvestre@ucdavis.edu>
2011-01-11 19:21:31 +0000

    Added Roles Attribute Tests
    git-tfs-id: [https://tfs.caes.ucdavis.edu:8088/tfs]$/Commencement/Trunk/Commencement;C9047

---
+++ Commencement.Tests/Misc/RolesFiltersTests.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Commencement.Controllers.Filters;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Commencement.Tests.Misc
+{
+    [TestClass]
+    public class RolesFiltersTests
+    {
+        [TestMethod]
+        public void TestAdminOnlyReturnsAdminRole()
+        {
+            #region Arrange
+            var attribute = new AdminOnlyAttribute();
+            #endregion Arrange
+
+            #region Assert
+            Assert.AreEqual("Admin", attribute.Roles);
+            #endregion Assert		
+        }
+        [TestMethod]
+        public void TestUserOnlyReturnsAdminRole()
+        {
+            #region Arrange
+            var attribute = new UserOnlyAttribute();
+            #endregion Arrange
+
+            #region Assert
+            Assert.AreEqual("User", attribute.Roles);
+            #endregion Assert
+        }
+        [TestMethod]
+        public void TestAnyoneWithRoleReturnsAdminRole()
+        {
+            #region Arrange
+            var attribute = new AnyoneWithRoleAttribute();
+            #endregion Arrange
+
+            #region Assert
+            Assert.AreEqual("Admin,User", attribute.Roles);
+            #endregion Assert
+        }
+        [TestMethod]
+        public void TestEmulationUserOnlyReturnsAdminRole()
+        {
+            #region Arrange
+            var attribute = new EmulationUserOnlyAttribute();
+            #endregion Arrange
+
+            #region Assert
+            Assert.AreEqual("EmulationUser", attribute.Roles);
+            #endregion Assert
+        }
+    }
+}
