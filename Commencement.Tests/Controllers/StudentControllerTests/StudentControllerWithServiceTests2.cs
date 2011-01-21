dd3f94ad83c65ef4eb941d39a42fd08b8d19dc71
Sylvestre, Jason <jsylvestre@ucdavis.edu>
2011-01-21 22:21:48 +0000

    added test
    git-tfs-id: [https://tfs.caes.ucdavis.edu:8088/tfs]$/Commencement/Trunk/Commencement;C9146

--- Commencement.Tests/Controllers/StudentControllerTests/StudentControllerWithServiceTests2.cs
+++ Commencement.Tests/Controllers/StudentControllerTests/StudentControllerWithServiceTests2.cs
@@ -1,18 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Security.Principal;
-using System.Web.Mvc;
-using Commencement.Controllers;
-using Commencement.Controllers.Filters;
 using Commencement.Controllers.Helpers;
-using Commencement.Controllers.Services;
 using Commencement.Controllers.ViewModels;
 using Commencement.Core.Domain;
 using Commencement.Tests.Core.Extensions;
 using Commencement.Tests.Core.Helpers;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using MvcContrib.TestHelper;
 using Rhino.Mocks;
 using UCDArch.Core.PersistanceSupport;
 
@@ -527,6 +521,92 @@ namespace Commencement.Tests.Controllers.StudentControllerTests
             #endregion Assert
         }
 
+
+        [TestMethod]
+        public void TestUpdateRegistration7()
+        {
+            //Test expected fields get copied 
+            #region Arrange
+            var collegeRepository = MockRepository.GenerateStub<IRepositoryWithTypedId<College, string>>();
+            RegistrationPopulator = new RegistrationPopulator(SpecialNeedRepository, RegistrationPetitionRepository, ParticipationRepository, RegistrationRepository);
+
+            var student = CreateValidEntities.Student(1);
+            var student2 = CreateValidEntities.Student(2);
+            ControllerRecordFakes.FakeCeremony(2, CeremonyRepository);
+            ControllerRecordFakes.FakeCollege(2, collegeRepository);
+            var registration1 = CreateValidEntities.Registration(1);
+            registration1.Student = student;
+            registration1.Address1 = "Address11";
+            registration1.Address2 = "Address21";
+            registration1.City = "City1";
+            registration1.Email = "1@email.com";
+            registration1.GradTrack = false;
+            registration1.MailTickets = false;
+            registration1.State = CreateValidEntities.State(1);
+            registration1.TermCode = CreateValidEntities.TermCode(1);
+            registration1.Zip = "1";
+
+
+
+            var registration2 = CreateValidEntities.Registration(2);
+            registration2.Email = string.Empty;
+
+
+            var registrationPostModel = new RegistrationPostModel();
+            registrationPostModel.Registration = registration2;
+
+
+            var registrationParticipations = new List<RegistrationParticipation>();
+            registrationParticipations.Add(CreateValidEntities.RegistrationParticipation(1));
+            registrationParticipations.Add(CreateValidEntities.RegistrationParticipation(2));
+            registrationParticipations[0].Ceremony = CeremonyRepository.GetNullableById(1);
+            registrationParticipations[0].Registration = CreateValidEntities.Registration(1);
+            registrationParticipations[0].Registration.Student = student2;
+            registrationParticipations[0].DateUpdated = DateTime.Now.AddDays(-10);
+            registrationParticipations[1].Ceremony = CeremonyRepository.GetNullableById(2);
+            registrationParticipations[1].Registration = CreateValidEntities.Registration(2);
+            registrationParticipations[1].Registration.Student = student2;
+            registrationParticipations[1].DateUpdated = DateTime.Now.AddDays(-11);
+            ControllerRecordFakes.FakeRegistrationParticipation(0, ParticipationRepository, registrationParticipations);
+
+            var participations = new List<CeremonyParticipation>();
+            for (int i = 0; i < 2; i++)
+            {
+                participations.Add(new CeremonyParticipation());
+                participations[i].ParticipationId = ParticipationRepository.GetNullableById(i+1).Id;
+                participations[i].Participate = true;
+                participations[i].Petition = false;
+                participations[i].Ceremony = CeremonyRepository.GetNullableById(i + 1);
+                participations[i].Major = CreateValidEntities.MajorCode(i + 1);
+                participations[i].Major.College = collegeRepository.GetNullableById((i+1).ToString()); 
+            }
+            registrationPostModel.CeremonyParticipations = participations;
+
+            var registrationPetitions = new List<RegistrationPetition>();
+            registrationPetitions.Add(CreateValidEntities.RegistrationPetition(1));
+            registrationPetitions.Add(CreateValidEntities.RegistrationPetition(2));
+            registrationPetitions[0].Ceremony = CeremonyRepository.GetNullableById(1);
+            registrationPetitions[0].Registration = CreateValidEntities.Registration(1);
+            registrationPetitions[0].Registration.Student = student2;
+            registrationPetitions[1].Ceremony = CeremonyRepository.GetNullableById(2);
+            registrationPetitions[1].Registration = CreateValidEntities.Registration(2);
+            registrationPetitions[1].Registration.Student = student2;
+            RegistrationPetitionRepository.Expect(a => a.Queryable)
+                .Return(registrationPetitions.AsQueryable()).Repeat.Any();
+
+            registration1.RegistrationParticipations = registrationParticipations;
+            #endregion Arrange
+
+            #region Act
+            RegistrationPopulator.UpdateRegistration(registration1, registrationPostModel, null, Controller.ModelState);
+            #endregion Act
+
+            #region Assert
+            Assert.IsTrue(Controller.ModelState.IsValid);
+            Assert.AreEqual(DateTime.Now.Date, registration1.RegistrationParticipations[0].DateUpdated.Date);
+            Assert.AreEqual(DateTime.Now.Date, registration1.RegistrationParticipations[1].DateUpdated.Date);
+            #endregion Assert
+        }
         #endregion UpdateRegistration Tests
         #endregion RegistrationPopulator Tests
     }
