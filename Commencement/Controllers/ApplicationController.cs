9ed3ffe6633cf9c763fdc78be3441a63dfc0b0d3
Alan Lai <anlai@ucdavis.edu>
2011-03-30 11:13:29 -0700

    added version attribute

--- Commencement/Controllers/ApplicationController.cs
+++ Commencement/Controllers/ApplicationController.cs
@@ -1,8 +1,10 @@
-﻿using Commencement.Core.Resources;
+﻿using Commencement.Controllers.Filters;
+using Commencement.Core.Resources;
 using UCDArch.Web.Controller;
 
 namespace Commencement.Controllers
 {
+    [LoadDisplayData]
     public class ApplicationController : SuperController {
     
         private string EmulationKey = StaticIndexes.EmulationKey;
