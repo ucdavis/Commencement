7f97dc5d76508fe504825d4db170a59a8a3e383c
Alan Lai <anlai@ucdavis.edu>
2011-04-06 16:33:56 -0700

    updated error reporting to just send an email rather than the async non-sense.

--- Commencement/Controllers/Services/ErrorService.cs
+++ Commencement/Controllers/Services/ErrorService.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Mail;
 using System.Web;
 
 namespace Commencement.Controllers.Services
@@ -16,10 +17,25 @@ namespace Commencement.Controllers.Services
 
         public void ReportError(Exception ex)
         {
-            var deligate = new BeginReportErrorHandlerDelegate(BeginReportErrorHandler);
-            var callback = new AsyncCallback(EndReportErrorHandler);
+            //var deligate = new BeginReportErrorHandlerDelegate(BeginReportErrorHandler);
+            //var callback = new AsyncCallback(EndReportErrorHandler);
 
-            deligate.BeginInvoke(ex, callback, null);
+            //deligate.BeginInvoke(ex, callback, null);
+
+            var client = new SmtpClient("smtp.ucdavis.edu");
+
+            var message = new MailMessage("automatedemail@caes.ucdavis.edu", "anlai@ucdavis.edu");
+            message.Subject = "Commencement Error";
+            message.Body = ex.Message;
+            message.IsBodyHtml = true;
+
+            if (ex.InnerException != null)
+            {
+                message.Body += "<br/><hr><br/>";
+                message.Body += ex.InnerException.Message;
+            }
+
+            client.Send(message);
         }
 
         public static void BeginReportErrorHandler(Exception ex)
