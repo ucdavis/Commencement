a5921f4a5fab1792bc74546bd632d338bce51836
anlai <anlai@ucdavis.edu>
2012-04-12 09:02:04 -0700

    updated the view to only show users with commencement access

--- Commencement.Db/Schema Objects/Schemas/dbo/Views/vUsers.view.sql
+++ Commencement.Db/Schema Objects/Schemas/dbo/Views/vUsers.view.sql
@@ -1,5 +1,9 @@
-﻿CREATE VIEW [dbo].[vUsers]
+﻿CREATE VIEW dbo.vUsers
 AS
-SELECT     UserID AS id, LoginID, Email, Phone, FirstName, LastName, EmployeeID, SID, UserKey
-FROM         Catbert3.dbo.Users
-WHERE     (Inactive = 0)
+SELECT        UserID AS id, LoginID, Email, Phone, FirstName, LastName, EmployeeID, SID, UserKey
+FROM            Catbert3.dbo.Users
+WHERE        (Inactive = 0) AND (UserID IN
+                             (SELECT        p.UserID
+                               FROM            Catbert3.dbo.Permissions AS p INNER JOIN
+                                                         Catbert3.dbo.Applications AS a ON p.ApplicationID = a.ApplicationID
+                               WHERE        (a.Name = 'Commencement') AND (p.Inactive = 0)))
