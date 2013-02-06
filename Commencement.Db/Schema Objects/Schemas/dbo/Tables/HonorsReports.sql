81cdb53506300d76a59994fcb9f27aca834e488b
Alan Lai <anlai@ucdavis.edu>
2013-02-06 07:35:33 -0800

    updated to match db

--- Commencement.Db/Schema Objects/Schemas/dbo/Tables/HonorsReports.sql
+++ Commencement.Db/Schema Objects/Schemas/dbo/Tables/HonorsReports.sql
@@ -2,7 +2,7 @@
 (
 	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
     [Contents] VARBINARY(MAX) NULL, 
-    [DateRequested] DATETIME NOT NULL DEFAULT getdate(), 
+    [DateRequested] DATETIME NOT NULL DEFAULT (getdate()), 
     [UserId] INT NOT NULL, 
     [TermCode] VARCHAR(6) NOT NULL, 
     [Honors4590] DECIMAL(18, 3) NOT NULL, 
--- Commencement.Db/Schema Objects/Schemas/dbo/Tables/TransferRequests.sql
+++ Commencement.Db/Schema Objects/Schemas/dbo/Tables/TransferRequests.sql
@@ -4,9 +4,9 @@
     [RegistrationParticipationId] INT NOT NULL, 
     [CeremonyId] INT NOT NULL, 
 	[Reason] varchar(200) not null,
-    [DateRequested] DATETIME NOT NULL DEFAULT getdate(), 
+    [DateRequested] DATETIME NOT NULL DEFAULT (getdate()), 
     [UserId] INT NOT NULL, 
-    [Pending] BIT NOT NULL DEFAULT 1, 
+    [Pending] BIT NOT NULL DEFAULT ((1)), 
     CONSTRAINT [FK_TransferRequest_RegistrationParticipations] FOREIGN KEY ([RegistrationParticipationId]) REFERENCES [RegistrationParticipations]([Id]), 
     CONSTRAINT [FK_TransferRequest_Ceremonies] FOREIGN KEY ([CeremonyId]) REFERENCES [Ceremonies]([Id])
 
