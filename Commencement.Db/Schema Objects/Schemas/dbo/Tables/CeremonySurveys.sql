b9c40e6e51e45465830ae80f6700d8f287f2a72a
Jason Sylvestre <jsylvestre@ucdavis.edu>
2013-07-18 11:06:08 -0700

    Created table. NOTE!!! DB Change

---
+++ Commencement.Db/Schema Objects/Schemas/dbo/Tables/CeremonySurveys.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[CeremonySurveys]
+(
+	[id] INT NOT NULL PRIMARY KEY, 
+    [CeremonyId] INT NOT NULL, 
+    [CollegeId] CHAR(2) NOT NULL, 
+    [SurveyUrl] VARCHAR(MAX) NULL, 
+    [SurveyId] INT NULL, 
+    CONSTRAINT [FK_CeremonySurveys_Ceremonies] FOREIGN KEY ([CeremonyId]) REFERENCES [Ceremonies]([id]), 
+    CONSTRAINT [FK_CeremonySurveys_Colleges] FOREIGN KEY ([CollegeId]) REFERENCES [Colleges]([id]), 
+    CONSTRAINT [FK_CeremonySurveys_Surveys] FOREIGN KEY ([SurveyId]) REFERENCES [Surveys]([Id]) 
+)
