937d5d3dc6de7b12f4c131e1a4082e2e38b2852e
Jason Sylvestre <jsylvestre@ucdavis.edu>
2014-01-17 10:16:56 -0800

    Created first pass of table.
    The Ceremony selection would be because there may not be an open one when this is requested.
    Existing form can be found here:
    http://www.caes.ucdavis.edu/students/current/commencement/visa

---
+++ Commencement.Db/Schema Objects/Schemas/dbo/Tables/VisaLetters.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE [dbo].[VisaLetters]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [Student_Id] UNIQUEIDENTIFIER NOT NULL, 
+	[DateCreated] DATETIME NOT NULL,
+    [Gender] CHAR NOT NULL, 
+    [Ceremony] CHAR NULL, 
+    [RelativeFirstName] VARCHAR(100) NOT NULL, 
+    [RelativeLastName] VARCHAR(100) NOT NULL, 
+    [RelationshipToStudent] VARCHAR(100) NOT NULL, 
+    [RelativeMailingAddress] VARCHAR(500) NOT NULL, 
+    [IsApproved] BIT NOT NULL DEFAULT 0, 
+    [DateApproved] DATETIME NULL    
+)
