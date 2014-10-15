a52ea435d040687907915504bb049275848f7fcc
Jason Sylvestre <jsylvestre@ucdavis.edu>
2014-10-15 11:10:33 -0700

    not a varbinary.

--- Commencement.Db/Schema Objects/Schemas/dbo/Tables/VisaLetters.sql
+++ Commencement.Db/Schema Objects/Schemas/dbo/Tables/VisaLetters.sql
@@ -1,6 +1,6 @@
 ﻿CREATE TABLE [dbo].[VisaLetters]
 (
-	[Id] INT NOT NULL PRIMARY KEY, 
+	[Id] INT              IDENTITY (1, 1) NOT NULL,
 	[Student_Id] UNIQUEIDENTIFIER NOT NULL,
     [DateCreated] DATETIME2 NOT NULL, 
     [Gender] CHAR(1) NOT NULL, 
@@ -9,12 +9,13 @@
     [RelativeFirstName] VARCHAR(100) NOT NULL, 
     [RelativeLastName] VARCHAR(100) NOT NULL, 
     [RelationshipToStudent] VARCHAR(100) NOT NULL, 
-    [RelativeMailingAddress] VARBINARY(500) NOT NULL, 
+    [RelativeMailingAddress] VARCHAR(500) NOT NULL, 
     [IsApproved] BIT NOT NULL DEFAULT ((0)), 
     [IsPending] BIT NOT NULL DEFAULT ((1)), 
     [DateDecided] DATETIME NULL, 
     [CollegeName] VARCHAR(250) NULL, 
     [MajorName] VARCHAR(250) NULL, 
     [CeremonyDateTime] DATETIME NULL, 
+    CONSTRAINT [PK_VisaLetters] PRIMARY KEY ([Id]), 
     
 )
