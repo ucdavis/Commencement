16dbb170daea2338345cefe247265221e715048d
Jason Sylvestre <jsylvestre@ucdavis.edu>
2015-05-06 11:29:34 -0700

    Replace view looking into catbert with local users table.

--- Commencement.Db/Schema Objects/Schemas/dbo/Views/vUsers.view.sql
+++ Commencement.Db/Schema Objects/Schemas/dbo/Views/vUsers.view.sql
@@ -1,9 +1,4 @@
 ﻿CREATE VIEW dbo.vUsers
 AS
-SELECT        UserID AS id, LoginID, Email, Phone, FirstName, LastName, EmployeeID, SID, UserKey
-FROM            Catbert3.dbo.Users
-WHERE        (Inactive = 0) AND (UserID IN
-                             (SELECT        p.UserID
-                               FROM            Catbert3.dbo.Permissions AS p INNER JOIN
-                                                         Catbert3.dbo.Applications AS a ON p.ApplicationID = a.ApplicationID
-                               WHERE        (a.Name = 'Commencement') AND (p.Inactive = 0)))
+SELECT        CatbertUserId AS id, Id AS LoginID, Email, Phone, FirstName, LastName
+FROM            dbo.Users
