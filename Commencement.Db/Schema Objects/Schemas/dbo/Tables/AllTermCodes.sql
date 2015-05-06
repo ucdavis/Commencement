6aee5017a278d80e654845c83eaf793bb5c812cb
Jason Sylvestre <jsylvestre@ucdavis.edu>
2015-05-06 11:22:45 -0700

    All Term Codes (Copied table from students db)

---
+++ Commencement.Db/Schema Objects/Schemas/dbo/Tables/AllTermCodes.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[AllTermCodes]
+(
+	    [id]          VARCHAR (6)  NOT NULL,
+    [Description] VARCHAR (30) NULL,
+    [StartDate]   DATETIME     NULL,
+    [EndDate]     DATETIME     NULL,
+    [TypeCode]    VARCHAR (1)  NULL,
+    CONSTRAINT [PK_AllTermCodes] PRIMARY KEY CLUSTERED ([id] ASC)
+)
