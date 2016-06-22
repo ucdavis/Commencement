cf42e35775eebeaae401ecbb420926b99e5887ab
Scott Kirkland <srkirkland@gmail.com>
2016-06-22 10:46:21 -0700

    basics of new mvc project

---
+++ Commencement.Mvc/App_Start/UCDArchBootstrapper.cs
@@ -0,0 +1,42 @@
+using System.Web.Mvc;
+using Castle.Windsor;
+using Commencement.Core.Domain;
+using Microsoft.Practices.ServiceLocation;
+using Commencement.Mvc.App_Start;
+using Commencement.Mvc.Controllers;
+using UCDArch.Data.NHibernate;
+using UCDArch.Web.IoC;
+using UCDArch.Web.ModelBinder;
+
+[assembly: WebActivator.PreApplicationStartMethod(typeof(UCDArchBootstrapper), "PreStart")]
+namespace Commencement.Mvc.App_Start
+{
+    public class UCDArchBootstrapper
+    {
+        /// <summary>
+        /// PreStart for the UCDArch Application configures the model binding, db, and IoC container
+        /// </summary>
+        public static void PreStart()
+        {
+            ModelBinders.Binders.DefaultBinder = new UCDArchModelBinder();
+
+            NHibernateSessionConfiguration.Mappings.UseFluentMappings(typeof(Student).Assembly);
+            
+            IWindsorContainer container = InitializeServiceLocator();
+        }
+
+        private static IWindsorContainer InitializeServiceLocator()
+        {
+            IWindsorContainer container = new WindsorContainer();
+
+            ControllerBuilder.Current.SetControllerFactory(new WindsorControllerFactory(container));
+
+            container.RegisterControllers(typeof(HomeController).Assembly);
+            ComponentRegistrar.AddComponentsTo(container);
+
+            ServiceLocator.SetLocatorProvider(() => new WindsorServiceLocator(container));
+
+            return container;
+        }
+    }
+}
