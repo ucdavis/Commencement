0b88f99b78744e6c55ffff008b35ae01f1cb4dac
Jason Sylvestre <jsylvestre@ucdavis.edu>
2016-07-05 11:41:22 -0700

    Missing views that were in Donbot

---
+++ Commencement.Db/Schema Objects/Schemas/dbo/Views/vDuplicateRegistrations.sql
@@ -0,0 +1,9 @@
+﻿CREATE VIEW dbo.vDuplicateRegistrations
+AS
+SELECT        TOP (100) PERCENT COUNT(*) AS cnt, dbo.RegistrationParticipations.RegistrationId, dbo.RegistrationParticipations.CeremonyId, dbo.Students.StudentId
+FROM            dbo.RegistrationParticipations INNER JOIN
+                         dbo.Registrations ON dbo.RegistrationParticipations.RegistrationId = dbo.Registrations.id INNER JOIN
+                         dbo.Students ON dbo.Registrations.Student_Id = dbo.Students.Id
+GROUP BY dbo.RegistrationParticipations.RegistrationId, dbo.RegistrationParticipations.CeremonyId, dbo.Students.StudentId
+HAVING        (COUNT(*) > 1)
+ORDER BY cnt DESC
