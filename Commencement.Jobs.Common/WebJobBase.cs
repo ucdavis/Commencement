63b825c749de0526852f44b34f84f12f21e5edc5
Jason Sylvestre <jsylvestre@ucdavis.edu>
2016-08-31 09:57:29 -0700

    Db Access

---
+++ Commencement.Jobs.Common/WebJobBase.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Practices.ServiceLocation;
+using Ninject;
+using Ninject.Modules;
+
+namespace Commencement.Jobs.Common
+{
+    public abstract class WebJobBase
+    {
+        protected static IKernel ConfigureServices()
+        {
+            // register services
+            var kernel = new StandardKernel(new INinjectModule[] { new ServiceModule() });
+            //kernel.Components.Add<IInjectionHeuristic, PropertySetterInjectionHeuristic>();
+            ServiceLocator.SetLocatorProvider(() => new NinjectServiceLocator(kernel));
+
+            return kernel;
+        }
+    }
+}
