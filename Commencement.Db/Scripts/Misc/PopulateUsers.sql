430c086f3613965f70ef0f797bb749ad3b9b1ae0
Jason Sylvestre <jsylvestre@ucdavis.edu>
2016-09-01 13:44:01 -0700

    Added scripts for this update so they are in source control

---
+++ Commencement.Db/Scripts/Misc/PopulateUsers.sql
@@ -0,0 +1,13 @@
+﻿USE [Commencement]
+GO
+
+INSERT INTO [dbo].Users
+SELECT LoginID AS id, Email, Email AS UserEmail, Phone, FirstName, LastName, 1 AS IsActive, UserID AS CatbertUserId
+FROM Catbert3.dbo.Users
+WHERE (Inactive = 0) AND (UserID IN
+(SELECT p.UserID
+FROM Catbert3.dbo.Permissions AS p INNER JOIN
+Catbert3.dbo.Applications AS a ON p.ApplicationID = a.ApplicationID
+WHERE (a.Name = 'Commencement') AND (p.Inactive = 0)))
+
+GO
