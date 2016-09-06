db7999ed01c51e6af73d6423753ea045c816e4fb
Jason Sylvestre <jsylvestre@ucdavis.edu>
2016-09-06 10:35:05 -0700

    WIP get the emails

--- Commencement.Jobs.NotificationsCommon/Program.cs
+++ Commencement.Jobs.NotificationsCommon/Program.cs
@@ -3,20 +3,47 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Commencement.Core.Services;
 using Microsoft.Azure.WebJobs;
+using Microsoft.WindowsAzure;
+using Dapper;
 
 namespace Commencement.Jobs.NotificationsCommon
 {
-    // To learn more about Microsoft Azure WebJobs SDK, please see http://go.microsoft.com/fwlink/?LinkID=320976
-    class Program
+    public static class ProcessNotifications
     {
-        // Please set the following connection strings in app.config for this WebJob to run:
-        // AzureWebJobsDashboard and AzureWebJobsStorage
-        static void Main()
+        static readonly string SparkPostApiKey = CloudConfigurationManager.GetSetting("SparkPostApiKey");
+
+        public static void ProcessEmails(IDbService dbService, bool immediate)
         {
-            var host = new JobHost();
-            // The following code ensures that the WebJob will be running continuously
-            host.RunAndBlock();
-        }
+            var sendEmail = CloudConfigurationManager.GetSetting("send-email");
+            var testEmail = CloudConfigurationManager.GetSetting("send-test-emails");
+
+            //Don't execute unless email is turned on
+            if (!string.Equals(sendEmail, "Yes", StringComparison.InvariantCultureIgnoreCase))
+            {
+                Console.WriteLine("No emails sent because send-email is not set to 'Yes'");
+                return;
+            }
+
+            using (var connection = dbService.GetConnection())
+            {
+                List<dynamic> pending = connection.Query(@"
+                    select emailqueue.id, [subject], [body]
+                        ,case 
+				            when registrations.email is null then students.email
+				            else students.email + ';' + registrations.email
+				        end as emails
+		            from emailqueue
+			            inner join Students on students.Id = EmailQueue.Student_Id
+			            LEFT OUTER JOIN Registrations on registrations.id = EmailQueue.RegistrationId
+		            where Pending = 1 and [immediate] = @immediate").ToList();
+
+                foreach (var email in pending)
+                {
+                    var subject = email.subject;
+
+                }
+            }
     }
 }
