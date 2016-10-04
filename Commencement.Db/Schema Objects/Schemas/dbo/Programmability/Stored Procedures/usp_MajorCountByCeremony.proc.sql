a0b0848e08d845bdc847bf5b6344be4af966c18a
Jason Sylvestre <jsylvestre@ucdavis.edu>
2016-10-04 14:16:04 -0700

    Add the major code to the sproc (cleaner)

--- Commencement.Db/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/usp_MajorCountByCeremony.proc.sql
+++ Commencement.Db/Schema Objects/Schemas/dbo/Programmability/Stored Procedures/usp_MajorCountByCeremony.proc.sql
@@ -8,7 +8,7 @@ AS
 	Count of students by major
 */
 
-select count(rp.id) num, isnull(m.fullname, m.name + '[Banner Name]') name, m.CollegeCode, c.datetime
+select count(rp.id) num, m.id as majorcode, isnull(m.fullname, m.name + '[Banner Name]') name, m.CollegeCode, c.datetime
 from registrationparticipations rp
 	inner join registrations r on r.id = rp.registrationid
 	inner join students s on r.student_id = s.id
@@ -17,7 +17,7 @@ from registrationparticipations rp
 where rp.ceremonyid = @ceremonyId
   and cancelled = 0
   and sjablock = 0 and blocked = 0
-group by m.fullname, m.name, m.CollegeCode, c.datetime
+group by m.fullname, m.name, m.id, m.CollegeCode, c.datetime
 order by m.name
 
 RETURN 0
