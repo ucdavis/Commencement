72383255722102304a9843dbcb363a46940973fe
Jason Sylvestre <jsylvestre@ucdavis.edu>
2016-10-31 14:35:16 -0700

    Packages to jobs.

--- Commencement.Mvc/Global.asax.cs
+++ Commencement.Mvc/Global.asax.cs
@@ -16,6 +16,7 @@ namespace Commencement.Mvc
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+            LogConfig.ConfigureLogging();
         }
     }
 }
