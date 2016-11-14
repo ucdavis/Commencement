d66ff490d128a179e5f5bd49a190d0d41c73f7bb
Jason Sylvestre <jsylvestre@ucdavis.edu>
2016-11-14 11:30:10 -0800

    Scott's code....

--- Commencement.Mvc/App_Start/FilterConfig.cs
+++ Commencement.Mvc/App_Start/FilterConfig.cs
@@ -1,4 +1,5 @@
-﻿using System.Web;
+﻿using System;
+using System.Web;
 using System.Web.Mvc;
 
 namespace Commencement.Mvc
@@ -7,7 +8,18 @@ namespace Commencement.Mvc
     {
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
-            //filters.Add(new HandleErrorAttribute()); //Seems to prevent Stackify exception logging
+            filters.Add(new HandleAndLogErrorAttribute());
+        }
+    }
+
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method,
+    Inherited = true, AllowMultiple = true)]
+    public class HandleAndLogErrorAttribute : HandleErrorAttribute
+    {
+        public override void OnException(ExceptionContext filterContext)
+        {
+            // log exception here via stackify
+            base.OnException(filterContext);
         }
     }
 }
