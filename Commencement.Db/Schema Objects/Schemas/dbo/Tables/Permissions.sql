c635af37e24b39d916995127a9e7e8deb1615324
Jason Sylvestre <jsylvestre@ucdavis.edu>
2017-01-10 09:45:17 -0800

    Comparing against azure, these two tables were different.

--- Commencement.Db/Schema Objects/Schemas/dbo/Tables/Permissions.sql
+++ Commencement.Db/Schema Objects/Schemas/dbo/Tables/Permissions.sql
@@ -1,6 +1,6 @@
-﻿CREATE TABLE [dbo].[Permissions]
-(
-	[RoleId] CHAR(2) NOT NULL , 
-    [UserId] VARCHAR(10) NOT NULL, 
-    PRIMARY KEY ([UserId], [RoleId])
-)
+﻿CREATE TABLE [dbo].[Permissions] (
+    [RoleId] CHAR (2)     NOT NULL,
+    [UserId] VARCHAR (10) NOT NULL,
+    CONSTRAINT [PK__Permissi__8AFACE1A2BC97F7C] PRIMARY KEY CLUSTERED ([RoleId] ASC, [UserId] ASC)
+);
+
