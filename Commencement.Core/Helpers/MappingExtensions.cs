336efe55ade9536a7e9655d56ce8a604252c8b2c
Jason Sylvestre <jsylvestre@ucdavis.edu>
2017-01-23 14:25:33 -0800

    Use extension

---
+++ Commencement.Core/Helpers/MappingExtensions.cs
@@ -0,0 +1,12 @@
+﻿using FluentNHibernate.Mapping;
+
+namespace Commencement.Core.Helpers
+{
+    public static class MappingExtensions
+    {
+        public static PropertyPart StringMaxLength(this PropertyPart propertyPart)
+        {
+            return propertyPart.CustomType("StringClob");
+        }
+    }
+}
