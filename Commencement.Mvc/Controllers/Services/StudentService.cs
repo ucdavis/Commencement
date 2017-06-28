ed3015bdcb36802b6ac710abee090ee5577c6c53
Jason Sylvestre <jsylvestre@ucdavis.edu>
2017-06-28 09:27:17 -0700

    calls into the student service

--- Commencement.Mvc/Controllers/Services/StudentService.cs
+++ Commencement.Mvc/Controllers/Services/StudentService.cs
@@ -1,11 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
+using System.Net;
 using System.Security.Principal;
+using System.Text;
 using Commencement.Controllers.Helpers;
 using Commencement.Core.Domain;
 using Commencement.Core.Helpers;
 using Commencement.Core.Resources;
+using Newtonsoft.Json;
 using UCDArch.Core.PersistanceSupport;
 using UCDArch.Core.Utils;
 using UCDArch.Data.NHibernate;
@@ -19,7 +23,6 @@ namespace Commencement.Controllers.Services
         Registration GetPriorRegistration(Student student, TermCode termCode) ;
         Student BannerLookupByLogin(string login);
         Student BannerLookup(string studentId);
-        Student BannerLookupName(string studentId);
         bool CheckExisting(string login, TermCode term, string studentId = null);
     }
 
@@ -29,6 +32,8 @@ namespace Commencement.Controllers.Services
         private readonly IRepository<Ceremony> _ceremonyRepository;
         private readonly IRepository<Registration> _registrationRepository;
         private readonly IRepositoryWithTypedId<MajorCode, string> _majorRepository;
+        private readonly string _studentServiceUrl = ConfigurationManager.AppSettings["StudentServiceUrl"];
+        private readonly string _key = ConfigurationManager.AppSettings["StudentServiceKey"];
 
         private Student CurrentStudent
         {
@@ -48,25 +53,24 @@ namespace Commencement.Controllers.Services
         {
             var currentStudent = _studentRepository.Queryable.SingleOrDefault(x => x.Login == currentUser.Identity.Name && x.TermCode == TermService.GetCurrent());
             
+            //We are not doing a banner lookup as it should not be necessary as we are grabbing students with 120 units or more -- Update 2017-06-28 do it because the ones that are missed
+            if (currentStudent == null)
+            {
+                var student = BannerLookupByLogin(currentUser.Identity.Name);
 
-            //We are not doing a banner lookup as it should not be necessary as we are grabbing students with 120 units or more
-            //if (currentStudent == null)
-            //{
-            //    var student = BannerLookupByLogin(currentUser.Identity.Name);
+                // student still doesn't exist
+                if (!_studentRepository.Queryable.Any(a => a.Login == currentUser.Identity.Name && a.TermCode == TermService.GetCurrent()))
+                {
+                    if (student != null)
+                    {
+                        student.AddedBy = currentUser.Identity.Name;
+                        _studentRepository.EnsurePersistent(student);
+                        Check.Require(_studentRepository.Queryable.Count(a => a.Login == currentUser.Identity.Name && a.TermCode == TermService.GetCurrent()) == 1, "There were too many students returned.");
+                    }
 
-            //    // student still doesn't exist
-            //    if (!_studentRepository.Queryable.Any(a => a.Login == currentUser.Identity.Name && a.TermCode == TermService.GetCurrent()))
-            //    {
-            //        if (student != null)
-            //        {
-            //            student.AddedBy = currentUser.Identity.Name;
-            //            _studentRepository.EnsurePersistent(student);
-            //            Check.Require(_studentRepository.Queryable.Count(a => a.Login == currentUser.Identity.Name && a.TermCode == TermService.GetCurrent()) == 1, "There were too many students returned.");
-            //        }
-
-            //        currentStudent = student;       
-            //    }
-            //}
+                    currentStudent = student;
+                }
+            }
 
             return currentStudent; // if it returns null, the search didn't yield any results
         }
@@ -98,44 +102,81 @@ namespace Commencement.Controllers.Services
 
         public Student BannerLookupByLogin(string login)
         {
-            var searchQuery = NHibernateSessionManager.Instance.GetSession().CreateSQLQuery(StaticValues.StudentService_BannerLookupByLogin_SQL);
-            searchQuery.SetString("login", login);
-            searchQuery.AddEntity(typeof (BannerStudent));
-            searchQuery.SetTimeout(90);
-            var result = searchQuery.List<BannerStudent>();
-            return ExtractStudentFromResult(result);
+            if (string.IsNullOrWhiteSpace(login))
+            {
+                return null;
+            }
+            using (var webClient = new WebClient())
+            {
+                var parameters = new List<KeyValuePair<string, string>>
+                {
+                    new KeyValuePair<string, string>("login", login),
+                    new KeyValuePair<string, string>("key", _key)
+                };
+                var json = webClient.DownloadString(GenerateUrl(_studentServiceUrl, "Student", "SearchStudentByLogin", parameters));
+
+                var bannerStudents = JsonConvert.DeserializeObject<List<BannerStudent>>(json);
+                return ExtractStudentFromResult(bannerStudents);
+            }
+
+
+
+            //var searchQuery = NHibernateSessionManager.Instance.GetSession().CreateSQLQuery(StaticValues.StudentService_BannerLookupByLogin_SQL);
+            //searchQuery.SetString("login", login);
+            //searchQuery.AddEntity(typeof (BannerStudent));
+            //searchQuery.SetTimeout(90);
+            //var result = searchQuery.List<BannerStudent>();
+            //return ExtractStudentFromResult(result);
         }
 
         public Student BannerLookup(string studentId)
         {
-            var searchQuery = NHibernateSessionManager.Instance.GetSession().CreateSQLQuery(StaticValues.StudentService_BannerLookup_SQL);
-            searchQuery.SetString("studentid", studentId);
-            searchQuery.AddEntity(typeof (BannerStudent));
-            searchQuery.SetTimeout(90);
-
-            var result = searchQuery.List<BannerStudent>();
-            return ExtractStudentFromResult(result);
-        }
-
-        public Student BannerLookupName(string studentId)
-        {
-            var searchQuery = NHibernateSessionManager.Instance.GetSession().CreateSQLQuery(StaticValues.StudentService_BannerLookupName_SQL);
-            searchQuery.SetString("studentid", studentId);
-            searchQuery.AddEntity(typeof(BannerName));
-            searchQuery.SetTimeout(90);
-
-            var result = searchQuery.List<BannerName>();
-            //return ExtractStudentFromResult(result);
-
-            var r1 = result.FirstOrDefault();
-            if (r1 != null)
+            if (string.IsNullOrWhiteSpace(studentId))
             {
-                var student = new Student() { StudentId = studentId, Pidm = r1.Pidm, FirstName = r1.FirstName, MI = r1.MI, LastName = r1.LastName, Login = r1.LoginId, Email = r1.Email };
-                return student;
+                return null;
+            }
+            using (var webClient = new WebClient())
+            {
+                var parameters = new List<KeyValuePair<string, string>>
+                {
+                    new KeyValuePair<string, string>("studentid", studentId),
+                    new KeyValuePair<string, string>("key", _key)
+                };
+                var json = webClient.DownloadString(GenerateUrl(_studentServiceUrl, "Student", "SearchStudent", parameters));
+
+                var bannerStudents = JsonConvert.DeserializeObject<List<BannerStudent>>(json);
+                return ExtractStudentFromResult(bannerStudents);
             }
 
-            return null;
+
+            //var searchQuery = NHibernateSessionManager.Instance.GetSession().CreateSQLQuery(StaticValues.StudentService_BannerLookup_SQL);
+            //searchQuery.SetString("studentid", studentId);
+            //searchQuery.AddEntity(typeof (BannerStudent));
+            //searchQuery.SetTimeout(90);
+
+            //var result = searchQuery.List<BannerStudent>();
+            //return ExtractStudentFromResult(result);
         }
+
+        private string GenerateUrl(string url, string controller, string action, IEnumerable<KeyValuePair<string, string>> parameters)
+        {
+            var result = new StringBuilder(string.Format(url, controller));
+            result.Append(action);
+            result.Append("?");
+
+            foreach (var p in parameters)
+            {
+                if (result[result.Length - 1] != '?')
+                {
+                    result.Append("&");
+                }
+
+                result.Append(string.Format("{0}={1}", p.Key, p.Value));
+            }
+
+            return result.ToString();
+        }
+
 
         public Student ExtractStudentFromResult(IList<BannerStudent> results)
         {
@@ -145,6 +186,10 @@ namespace Commencement.Controllers.Services
             var student = new Student(r1.Pidm, r1.StudentId, r1.FirstName, r1.Mi, r1.LastName, r1.CurrentUnits, r1.EarnedUnits, r1.Email, r1.LoginId, TermService.GetCurrent());
             foreach (var a in results)
             {
+                if (r1.StudentId != a.StudentId)
+                {
+                    throw new Exception(string.Format("Multiple students with different studentids returned: {0}", r1.StudentId ));
+                }
                 var major = _majorRepository.GetNullableById(a.Major);
                 if (major != null && !student.Majors.Contains(major)) student.Majors.Add(major);
             }
@@ -230,10 +275,6 @@ namespace Commencement.Controllers.Services
             return GenerateFake();
         }
 
-        public Student BannerLookupName(string studentId)
-        {
-            throw new NotImplementedException();
-        }
 
         public Student ExtractStudentFromResult(IList<BannerStudent> results)
         {
