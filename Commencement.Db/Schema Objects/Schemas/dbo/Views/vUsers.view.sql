16bae01301db42b774644f4831c8bfafae5f80f5
Jason Sylvestre <jsylvestre@ucdavis.edu>
2018-09-04 11:03:18 -0700

    Update sql table

--- Commencement.Db/Schema Objects/Schemas/dbo/Views/vUsers.view.sql
+++ Commencement.Db/Schema Objects/Schemas/dbo/Views/vUsers.view.sql
@@ -1,4 +1,4 @@
 ﻿CREATE VIEW dbo.vUsers
 AS
-SELECT        CatbertUserId AS id, Id AS LoginID, Email, Phone, FirstName, LastName
+SELECT        CatbertUserId AS id, Id AS LoginID, Email, Phone, FirstName, LastName, IsActive
 FROM            dbo.Users
